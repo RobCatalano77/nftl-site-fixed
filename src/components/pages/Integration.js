6881ac922ffccc4c7da403a9342e45941565ec62
RobCatalano77 <robert.catalano1@marist.edu>
2022-02-04 01:44:26 -0500

    adding back in titles

--- src/components/pages/Integration.js
+++ src/components/pages/Integration.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Integration() {
   return (
     <>
+      <h1 className='integration'>Integration</h1>
     </>
   );
 }
--- src/components/pages/WMSSystem.js
+++ src/components/pages/WMSSystem.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function WMSSystem() {
   return (
     <>
+      <h1 className='wms-system'>WMS System</h1>
     </>
   );
 }
