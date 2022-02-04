54f1c821dd942db85109ec76565291fca7d79716
RobCatalano77 <robert.catalano1@marist.edu>
2022-02-04 01:54:23 -0500

    replacing 'sign up' with 'contact us'

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export function Button() {
   return (
     <Link to='contact-us'>
-      <button className='btn'>Sign Up</button>
+      <button className='btn'>Contact Us</button>
     </Link>
   );
 }
