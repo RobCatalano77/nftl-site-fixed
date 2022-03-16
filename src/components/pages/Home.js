a5a6da987cdaf7a58b5c0a26c7dd1a973c3fdb58
RobCatalano77 <robert.catalano1@marist.edu>
2022-03-15 20:55:13 -0400

    updating homepage to include slogan

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,9 @@ import '../../App.css';
 export default function Home() {
   return (
     <>
-      <h1 className='home'>NFT Logistics</h1>
+        <h1 className='heading'>NFT Logistics</h1>
+        <h2 className='subheading'>From big to small, we handle it all!</h2>
+      
     </>
   );
 }
